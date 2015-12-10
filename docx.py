e310b7e4bde6595ad5471e57a3cfd6306c6d5c06
Daniel <dancassig@gmail.com>
2015-12-10 17:32:57 -0600

    Creates document with set width and height

--- docx.py
+++ docx.py
@@ -1,3 +1,18 @@
+from docx import Document
+from docx.shared import Mm
+
 __author__ = 'daniel'
 
+document = Document()
+section = document.sections
 
+section.page_width = Mm(80)
+section.page_height = Mm(297)
+
+
+paragraph = document.add_paragraph('Lorem ipsum dolor sit amet.')
+document.add_heading('The REAL meaning of the universe')
+document.add_heading('The role of dolphins', level=2)
+
+
+document.save('test.docx')
