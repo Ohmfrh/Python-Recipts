ddb902c3cc28a50209f39a9cb853f71029e9d30e
Daniel <dancassig@gmail.com>
2015-12-10 13:22:17 -0600

    Renames file

---
+++ mergeReport.py
@@ -0,0 +1,26 @@
+import Image
+
+__author__ = 'daniel'
+
+
+def main():
+    # 1 B/W
+    # L Grayscale
+    # RGB
+    # RGBA
+    # CMYK
+    # YCbCr
+    # I
+    # F Float pixels
+    size = width, height = 320,240
+
+    image = Image.new("RGB", size, "white")
+    image.show()
+
+    recipt1 =
+
+    del image
+
+
+if __name__ == "__main__":
+    main()
