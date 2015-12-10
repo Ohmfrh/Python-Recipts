08b48b3f7d89d79459839e499019c8b972c7ddc4
Daniel <dancassig@gmail.com>
2015-12-10 13:35:37 -0600

    Pastes first slip

--- mergeReport.py
+++ mergeReport.py
@@ -4,22 +4,19 @@ __author__ = 'daniel'
 
 
 def main():
-    # 1 B/W
-    # L Grayscale
-    # RGB
-    # RGBA
-    # CMYK
-    # YCbCr
-    # I
-    # F Float pixels
-    size = width, height = 320,240
+    size = width, height = 950, 1500
+    area1 = (0, 0, 736, 862)
 
     image = Image.new("RGB", size, "white")
+
+    slip1 = Image.open('img/testout1.jpg')
+    slip2 = Image.open('img/testout2.jpg')
+
+    image.paste(slip1, area1)
+
     image.show()
 
-    recipt1 =
-
-    del image
+    del image, slip1, slip2
 
 
 if __name__ == "__main__":
