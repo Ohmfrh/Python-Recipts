6994a85cc819e5ff56b30889e4b06649e0f843d3
Daniel <dancassig@gmail.com>
2015-12-10 18:03:17 -0600

    Adds jpg2docx

--- createPrintable.py
+++ createPrintable.py
@@ -1,6 +1,7 @@
 from pdf2jpg import pdf2jpg
 from cropJPG import cropJPG
 from mergeReport import mergeReport
+from jpg2docx import jpg2docx
 
 __author__ = 'daniel'
 
@@ -8,6 +9,7 @@ def main():
     pdf2jpg()
     cropJPG()
     mergeReport()
+    jpg2docx()
 
     print "DONE"
 
