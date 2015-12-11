d294fd388dc6947f571844e92d8df5d2f4171e0f
Daniel <dancassig@gmail.com>
2015-12-10 18:01:33 -0600

    Creates class from jpg2docx

--- jpg2docx.py
+++ jpg2docx.py
@@ -3,18 +3,18 @@ from docx.shared import Mm
 
 __author__ = 'daniel'
 
-document = Document()
-sections = document.sections
-section = sections[0]
+def jpg2docx():
+    document = Document()
+    sections = document.sections
+    section = sections[0]
 
-section.page_width = Mm(80)
-section.page_height = Mm(297)
-section.top_margin = Mm(0)
-section.bottom_margin = Mm(0)
-section.left_margin = Mm(0)
-section.right_margin = Mm(0)
+    section.page_width = Mm(80)
+    section.page_height = Mm(297)
+    section.top_margin = Mm(0)
+    section.bottom_margin = Mm(0)
+    section.left_margin = Mm(0)
+    section.right_margin = Mm(0)
 
-document.add_picture('img/testReport.jpg', width=Mm(80))
+    document.add_picture('img/testReport.jpg', width=Mm(80))
 
-
-document.save('test.docx')
+    document.save('test.docx')
