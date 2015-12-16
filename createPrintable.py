db6e63ed478bb5f1e014798153b064b209e7fb7e
Daniel <dancassig@gmail.com>
2015-12-15 18:03:41 -0600

    Makes program autonomous

--- createPrintable.py
+++ createPrintable.py
@@ -2,24 +2,28 @@ from pdf2jpg import pdf2jpg
 from cropReport import *
 from mergeReport import mergeReport
 from jpg2docx import jpg2docx
-from directoryAccess import cleanup
+from directoryAccess import *
 
 __author__ = 'daniel'
+
 
 def main():
     box1 = (0, 0, 736, 862)
     box2 = (750, 462, 1700, 1100)
     box3 = (0, 90, 475, 300)
-    pdf = 'pdf/1.pdf'
-    doc_name = '1.docx'
+    folder = 'guide/'
 
-    pdf2jpg(pdf)
-    cropJPG(box1, 'tmp/test1.jpg', 'tmp/testout1.jpg')
-    cropJPG(box2, 'tmp/test1.jpg', 'tmp/testout2.jpg')
-    cropRw(box3, 'tmp/testout2.jpg')
-    mergeReport()
-    jpg2docx(doc_name)
-    cleanup()
+    files = getPDF(folder)
+
+    for fil in files:
+        doc_name = fil.split('.')
+        pdf2jpg(os.path.join(folder, fil))
+        cropJPG(box1, 'tmp/test1.jpg', 'tmp/testout1.jpg')
+        cropJPG(box2, 'tmp/test1.jpg', 'tmp/testout2.jpg')
+        cropRw(box3, 'tmp/testout2.jpg')
+        mergeReport()
+        jpg2docx('print/' + doc_name[0] + '.docx')
+        cleanup()
 
     print "DONE"
 
