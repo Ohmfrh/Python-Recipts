bcb57a1a0cf1f5741adee2e72835a7f9599e4af9
Daniel Castro <dancassig@gmail.com>
2016-04-03 19:14:22 -0500

    Cleans up main by moving code to new file

--- gprint.py
+++ gprint.py
@@ -1,41 +1,13 @@
-from pdf2jpg import pdf2jpg
-from cropReport import *
-from mergeReport import mergeReport
-from jpg2docx import jpg2docx
-from directoryAccess import *
-import time
+from guideControl import *
+
 
 __author__ = 'daniel'
 
 
 def main():
-    fedex()
+    test()
 
 
 if __name__ == "__main__":
     main()
 
-def fedex():
-    box1 = (0, 0, 736, 862)
-    box2 = (750, 462, 1700, 1100)
-    box3 = (0, 90, 505, 300)
-    folder = 'guide/'
-    i = 1.0
-    total_files = 0
-    current_time = time.strftime("%d_%m_%Y")
-
-    files = getPDF(folder)
-
-    total_files = len(files)
-
-    for fil in files:
-        doc_name = fil.split('.')
-        pdf2jpg(os.path.join(folder, fil))
-        cropJPG(box1, 'tmp/test1.jpg', 'tmp/testout1.jpg')
-        cropJPG(box2, 'tmp/test1.jpg', 'tmp/testout2.jpg')
-        cropRw(box3, 'tmp/testout2.jpg')
-        mergeReport()
-        jpg2docx('print/' + current_time + '_' + doc_name[0] + '.docx')
-        cleanup()
-        print repr((i/total_files)*100) + "%"
-        i += 1
