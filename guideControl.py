899db6155b785aff39179a5b0b247a52dfcda2cc
Daniel Castro <dancassig@gmail.com>
2016-04-03 19:57:18 -0500

    Ads function to print dhl files

--- guideControl.py
+++ guideControl.py
@@ -1,9 +1,12 @@
-from pdf2jpg import pdf2jpg
+from pdf2jpg import *
 from cropReport import *
 from mergeReport import mergeReport
 from jpg2docx import jpg2docx
 from directoryAccess import *
 import time
+import warnings
+
+warnings.filterwarnings('ignore')
 
 
 def fedex():
@@ -26,7 +29,30 @@ def fedex():
         cropJPG(box2, 'tmp/test1.jpg', 'tmp/testout2.jpg')
         cropRw(box3, 'tmp/testout2.jpg')
         mergeReport()
-        jpg2docx('print/' + current_time + '_' + doc_name[0] + '.docx')
+        jpg2docx('print/' + current_time + '_' + doc_name[0] + '.docx', 'tmp/testReport.jpg', 80)
         cleanup()
         print repr((i/total_files)*100) + "%"
         i += 1
+
+
+def dhl():
+    folder = 'guide/'
+    box1 = (80, 65, 800, 1400)
+    current_time = time.strftime("%d_%m_%Y")
+    i = 1.0
+    total_files = 0
+
+    files = getPDF(folder)
+
+    total_files = len(files)
+
+    for fil in files:
+        doc_name = fil.split('.')
+        pdf2jpgDHL(os.path.join(folder, fil))
+        cropJPG(box1, 'tmp/dhlimg.jpg', 'tmp/dhlfinal.jpg')
+        jpg2docx('print/' + current_time + '_' + doc_name[0] + '.docx', 'tmp/dhlfinal.jpg', 75)
+        cleanup()
+        print repr((i/total_files)*100) + "%"
+        i += 1
+
+
