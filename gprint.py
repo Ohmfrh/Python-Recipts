76ee3b6a8fadae47f5daccbaaccb3c03b2365c67
Daniel Castro <dancassig@gmail.com>
2016-04-03 20:17:31 -0500

    Now uses arguments to select dhl or fedex

--- gprint.py
+++ gprint.py
@@ -1,11 +1,23 @@
 from guideControl import *
+import sys
 
 
 __author__ = 'daniel'
 
 
 def main():
-    fedex()
+    try:
+        if sys.argv[1] == 'dhl':
+            dhl()
+        elif sys.argv[1] == 'fedex':
+            fedex()
+        else:
+            print "Invalid argument [",sys.argv[1],"]. Try with 'dhl' or 'fedex'."
+
+    except IndexError:
+        print 'Error: Not enough arguments\n Ex: python gprint.py [dhl or fedex]'
+    except:
+        print 'Unexpected Error:', sys.exc_info()
 
 
 if __name__ == "__main__":
